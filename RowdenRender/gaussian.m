6fec032882564937ce0175af3bb7480f5ba54c7b
Alex Rowden <alrowden@cs.umd.edu>
2020-04-07 17:47:32 -0400

    get fit in this branch

---
+++ RowdenRender/gaussian.m
@@ -0,0 +1,9 @@
+function [z] = gaussian(X, Y, sigma, mu_x, mu_y, amplitude)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+    X = X.';
+    z = amplitude * exp(-((X-mu_x).^2/(2 * sigma^2) + (Y - mu_y).^2/ (2 * sigma^2)));
+
+end
+
+ 
