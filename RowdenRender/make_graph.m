d22d16ca4cb93315a8bb8ec35ed0b76bfdf62608
Alex Rowden <alrowden@cs.umd.edu>
2020-04-08 11:37:12 -0400

    add make_graph

---
+++ RowdenRender/make_graph.m
@@ -0,0 +1,19 @@
+lon_range = min(Longitude):.00001:max(Longitude);
+lat_range = min(Latitude):.00001:max(Latitude);
+sig_data = zeros(length(lon_range), length(lat_range));
+sig_datas = containers.Map('all', sig_data);
+for index=1:size(unique_bssids, 1)  
+    vals = bssid_tables(index).gaussian;
+    if vals.sigma == -1
+        vals.sigma = avg_sigma / count;
+    end
+    z = gaussian(lon_range, lat_range, vals.sigma, vals.mu_x, vals.mu_y, vals.amplitude);
+    key = int2str(bssid_tables(index).table.Frequency(1));
+    if sig_datas.isKey(key)
+        sig_datas(key) = max(sig_datas(key), z);
+    else
+        sig_datas(key) = z;
+    end
+    sig_datas('all') = max(sig_datas('all'), z);
+end
+mesh(sig_datas('all')); 
